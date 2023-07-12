ac451ae2bd44b632f6b8133fc3a3c48cc589001b
Caiyuzhen <caiyuzhen00@gmail.com>
2023-07-13 01:12:39 +0800

    fix;buildBug

--- main.js
+++ main.js
@@ -18,7 +18,8 @@ app.on('ready', () => {
 
 	app.dock.setIcon('./src/resource/logo/magicApp@1x.png') // dock 栏的图标
 
-	const urlLoaction = isDev ? 'http://localhost:3000' : 'productUrl'//开发环境就是本地 3000 端口, 否则就是线上生产环境
+	// const urlLoaction = isDev ? 'http://localhost:3000' : 'productUrl'//开发环境就是本地 3000 端口, 否则就是线上生产环境
+	const urlLoaction = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`//开发环境就是本地 3000 端口, 在生产环境下,将 url 设置为编译打包后的前端静态资源所在位置
 
 	mainWindow.loadURL(urlLoaction) //加载 react 运行的 3000 端口
 })
