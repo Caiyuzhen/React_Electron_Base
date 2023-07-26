e79c2bf531a2520c29b4a3027f85ee594e83df5f
Caiyuzhen <caiyuzhen00@gmail.com>
2023-07-26 22:47:49 +0800

    feat:fileAction

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,7 @@
+const fs = window.require('fs').promises //🚀记得加上 window！
+// const path = window.require('path') 
+
+
 // 🚀 把 initFilesData 数组转为对象 id:{...}、 id:{} 格式的方式 
 export const mapArr = (arr) => {
 	return arr.reduce((map, item) => { // 🔥 reduce 方式是将数组快速转换为对象的方式
@@ -11,3 +15,59 @@ export const mapArr = (arr) => {
 export const objToArr = (obj) => {
 	return Object.keys(obj).map(key => obj[key]) // 🔥 Object.keys 取出对象所有的键,返回一个数组
 }
+
+
+
+
+
+// // 【👀读取】读取文件
+// export const readFile = (path) => {
+// 	return fs.readFile(path, 'utf8')
+// }
+
+
+// // 【✏️写入】写入文件
+// export const writeFile = (path, content) => {
+// 	return fs.writeFile(path, content, 'utf-8')
+// }
+
+
+// // 【🌫️修改】重命名
+// export const reNameFile = (path, newPath) => {
+// 	return fs.rename(path, newPath)
+// }
+
+
+// // 【🗑️删除】删除文件
+// export const deleteFile = (path) => {
+// 	return fs.unlink(path)
+// }
+
+
+// 👇测试
+// const testReadPath = path.join(__dirname, './mock/test.js') //读取路径
+// const testWritePath = path.join(__dirname, './mock/test.md') //写入路径
+// const oldPath = path.join(__dirname, './mock/rename.md') //重命名路径
+// const newPath = path.join(__dirname, './mock/renameNew.md') //重命名路径
+// const delPath = path.join(__dirname, './mock/del1.md') //删除路径
+
+
+// readFile(testReadPath).then((data) => {
+// 	console.log('读取成功:', data)
+// })
+
+
+// writeFile(testWritePath, '## 写入二级标题').then(() => {
+// 	console.log('写入成功')
+// })
+
+
+// reNameFile(oldPath, newPath).then(() => {
+// 	console.log('重命名成功')	
+// })
+
+
+// deleteFile(delPath).then(() => {
+// 	console.log('删除成功')
+// })
+
